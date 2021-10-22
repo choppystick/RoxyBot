5523fc583a169691e56978033e3d77252d0d3eb0
choppystick <93000287+choppystick@users.noreply.github.com>
2021-10-23 00:11:55 +0800

    Add files via upload

---
+++ utils/lists.py
@@ -0,0 +1,29 @@
+
+hatemessage = ["Stop this at once.", "You are hurting my heart.", "I miss Rudeus, he will take a better care of me.", "I have feelings too... **beep boop**",
+               "I hate you.", "I despise you.", "please stop.", "I can't believe you are doing this to me ;w;",
+               "So that's what you think of me huh..? sad ;-;", "Rudy, save me...", "You are an evil man.", "You monster.", "Please don't, I will be better."]
+
+imglink = ["https://i.imgur.com/JgTKcT9.jpg", "https://pbs.twimg.com/media/Eu6HCpFUUAA4VAv.jpg", "https://i.imgur.com/zxYyTrq.jpg",
+           "https://i.imgur.com/bWPQ3Cf.jpg", "https://i.imgur.com/P87LBnb.jpg", "https://i.imgur.com/b8vw4sG.jpg", "https://i.imgur.com/yvdyyHa.jpg",
+           "https://i.imgur.com/EcQ2auN.jpg", "https://i.imgur.com/QgUMPoV.jpg", "https://i.imgur.com/ViNvGRa.jpg", "https://i.imgur.com/LyEBUlM.jpg",
+           "https://i.imgur.com/lj9L8d4.jpg", "https://i.imgur.com/FVxEYLc.jpg", "https://i.imgur.com/o5DGc7r.jpg", "https://i.imgur.com/pd8ZNig.jpg",
+           "https://cdn.discordapp.com/attachments/651657631244484629/851765282128003072/roxycute.jpg", "https://i.imgur.com/E91a78R.jpg",
+           "https://www.kolpaper.com/wp-content/uploads/2021/04/Roxy-Migurdia-Wallpaper.jpg", "https://i.imgur.com/gy7ztTb.png",
+           "https://i.imgur.com/BWy7i6w.png", "https://i.imgur.com/YX36Th8.png", "https://i.imgur.com/YO0ARYc.jpg", "https://i.imgur.com/e9a98pD.jpg",
+           "https://i.imgur.com/5rH5oGu.jpg", "https://i.imgur.com/q7GpSuT.jpg", "https://i.imgur.com/BPfqh4w.jpg",
+           "https://i.imgur.com/4mOgOhZ.jpg", "https://i.imgur.com/oz3FXKK.png", "https://i.imgur.com/fQFWv0s.jpg", "https://i.imgur.com/GNnMliT.jpg",
+           "https://i.imgur.com/kbU9ivM.jpg", "https://i.imgur.com/PDDhv1j.jpg", "https://i.imgur.com/XB0NmCD.jpg", "https://i.imgur.com/YJ19AS8.jpg",
+           "https://i.imgur.com/KJ2bLqU.jpg", "https://i.imgur.com/wGE5ZIz.jpg", "https://i.imgur.com/DuTwNLC.jpg", "https://i.imgur.com/beigULB.jpg",
+           "https://i.imgur.com/aU4JKZx.jpg", "https://i.imgur.com/GG4ESAr.jpg", "https://i.imgur.com/wjRTNUh.jpg", "https://i.imgur.com/67UjAl7.jpg",
+           "https://i.imgur.com/ZGKl98q.jpg", "https://i.imgur.com/LVqdyRj.jpg", "https://i.imgur.com/UcAKfEC.jpg", "https://i.imgur.com/9nmbdgD.jpg",
+           "https://i.imgur.com/4vgcBAY.jpg", "https://i.imgur.com/QpDnyHB.jpg", "https://i.imgur.com/vuEwzUu.jpg", "https://i.imgur.com/xcS6kmD.jpg",
+           "https://i.imgur.com/NwJNXgQ.jpg", "https://i.imgur.com/0VyQZ24.jpg", "https://i.imgur.com/F7rISo0.jpg", "https://i.imgur.com/1LJHETC.png"]
+
+ballresponse = ['It is certain', 'It is decidedly so', 'Without a doubt', 'Yes, definitely', 'You may rely on it', 'As I see it, yes',
+                'Most likely', 'Outlook good', 'Yes', 'Signs point to yes', 'Reply hazy try again', 'Ask again later', "Might be possible",
+                'Better not tell you now', "100% yes", 'Cannot predict now', 'Concentrate and ask again',
+                "You'll be the judge", "Don't count on it", 'My reply is no', "Maybe no..."
+                'My sources say no', 'Outlook not so good', 'Very doubtful', "No", "Take a wild guess...",
+                "no... (╯°□°）╯︵ ┻━┻", "no... you baka"]
+
+response = {}
